97989aec4a619870d30659244dc384402ebd930b
MALAK E M ABED <memabed@students.iugaza.edu.ps>
2026-01-19 12:22:43 +0200

    Update tst.js

--- tst.js
+++ tst.js
@@ -1,4 +1,4 @@
-const password = "1234567890";
+const password = "123456789";
 console.log("Testing insecure code!", password); 
 const apikey = "123@456";
 console.log("Testing insecure code!", apikey);
